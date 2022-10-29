6eadb3d97ee9f80cafbb1c192aa821f5111e09eb
luismoro-yuca <luis.moro@yuca.live>
2022-10-28 22:38:11 -0300

    change setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
-import pathlib
 from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-  name="datalake_utils",
-  version="0.0.1",
-  description="Manipulate data on Amazon S3 using Apache Hadoop's filesystem compability",
-  long_description=long_description,
-  long_description_content_type="text/markdown",
-  author="Luis Roberto de Oliveira Moro",
-  author_email="",
-  license="MIT",
-  packages=["yuca-datalake-utils"],
-  zip_safe=False
+    name='datalake_utils',
+    version='1.0.0',
+    description="Manipulate data on Amazon S3 using Apache Hadoop filesystem compability",
+    py_modules=["datalake_utils"],
+    package_dir={'': 'datalake_utils'},
+    extras_require={
+        "dev": [
+            "pytest >= 3.7",
+            "check-manifest",
+            "twine"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Luis Roberto de Oliveira Moro",
+    # author_email="vaidhyanathan.sm@gmail.com",
+    # url="https://github.com/smv1999/number-utility"
 )
